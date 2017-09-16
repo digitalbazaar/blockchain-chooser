db0203a9f50c63c2b53bd5b07c1808d33eb9574f
Manu Sporny <msporny@digitalbazaar.com>
2017-09-16 13:45:13 -0400

    Add filter list component.

---
+++ src/filters/components/list/FiltersList.js
@@ -0,0 +1,13 @@
+// Notice that we do not have a controller since this component does not
+// have any specialized logic.
+export default {
+  name: 'filtersList',
+  config: {
+    bindings: {
+      filters: '<', 
+      selected : '<', 
+      showDetails : '&onSelected'
+    },
+    templateUrl: 'src/filters/components/list/FiltersList.html'
+  }
+};
